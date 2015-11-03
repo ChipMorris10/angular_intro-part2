bbddc290047b8081273834aaa100c312634c4285
ChipMorris10 <goldcomac@mac.com>
2015-11-02 18:49:01 -0700

    cleaning up old work

---
+++ reddit/main.js
@@ -0,0 +1,37 @@
+var app = angular.module("Reddit", []);
+app.controller("RedditController", function($scope){
+     $scope.posts = [
+     {
+      'title': "PUPPIES",
+       'author': "ME",
+       'imageURL': 'http://images.nationalgeographic.com/wpf/media-live/photos/000/347/cache/puppy-fun_34713_990x742.jpg',
+       'description': "Puppies having fun!"
+     },
+     {
+      'title': "CARS",
+       'author': "YOU",
+       'imageURL': "https://thedrivenblog.files.wordpress.com/2012/02/mp4-27_front-3-41.jpg",
+       'description': "Cool cars!"
+     },
+     {
+      'title': "CODING",
+       'author': "SOMEONE",
+       'imageURL': "http://www.mmemallette.com/wp-content/uploads/2015/02/coding.jpg",
+       'description': "All the Code!"
+     }
+     // ];
+     // $scope.addPosts = function(){
+     //   var payload = {
+     //   'title': $scope.title,
+     //   'author':$scope.author,
+     //   'imageURL': $scope.imageURL,
+     //   'description':$scope.description
+     };
+       $scope.posts.push(payload);
+       $scope.title = '';
+       $scope.author = '';
+       $scope.imageURL = '';
+       $scope.description = '';
+       console.log(payload);
+     };
+});
